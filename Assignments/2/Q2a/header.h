f5ea46f39a914a5bf5aca7371e8cb42c66f5d278
JeremyButt <jgb537@mun.ca>
2017-06-06 19:29:04 -0230

    finished Q2a

--- Assignments/2/Q2a/header.h
+++ Assignments/2/Q2a/header.h
@@ -25,47 +25,59 @@ public:
 class LLQueue : public Queue{
 
 public:
-  Node* rear_;
-  Node* front_;
+  Node* rear_;  //Node to hold the rear of the queue
+  Node* front_; //Node to hold the front of the queue
+
+  // c'tor and d'tor
   LLQueue(){
-    this->rear_ = nullptr;
-    this->front_ = nullptr;
+    this->rear_ = nullptr;  //initialize to nullptr
+    this->front_ = nullptr; //initialize to nullptr
   }
 
   ~LLQueue(){
-    this->rear_ = nullptr;
-    this->front_ = nullptr;
-    delete this;
+    this->rear_ = nullptr; //set to nullptr
+    this->front_ = nullptr; //set to nullptr
+    delete this->rear_; //delete
+    delete this->front_;  //delete
   }
 
+  // pre-condition:  given an integer 'e'
+  // post-condition: the queue will contain 'e' at one end of the data structure
   virtual void enqueue(int e) override{
-    Node* temp = new Node;
-    temp->data = e;
-    temp->next = nullptr;
+    Node* temp = new Node; // temp node
+    temp->data = e; //set data
+    temp->next = nullptr; // set next
+    //if front null, queue empty (node is new front)
     if(this->front_ == nullptr){
-      this->front_ = temp;
+      this->front_ = temp;  // temp is new front
     }else{
-      this->rear_->next = temp;
+      this->rear_->next = temp; //place behind front
     }
 
-    this->rear_ = temp;
+    this->rear_ = temp; //update rear to be new node
   }
 
+  // pre-condition:  queue must not be empty
+  // post-condition: the integer at the other end of the data structure is returned,
+  //                 and also removed from the structure
   virtual int dequeue() override{
-    int tempData;
-    Node* temp = new Node;
+    int tempData; //temp data var
+    Node* temp = new Node;  //temp node
+    // if front is null then empty
     if(this->front_ == nullptr){
       cout << "The LLQueue is empty!" << endl;
-      return 0;
+      return 0; // nothing to dequeue
     }else{
-      temp = this->front_;
-      this->front_ = this->front_->next;
-      tempData = temp->data;
-      delete temp;
-      return tempData;
+      temp = this->front_;  //temp to front
+      this->front_ = this->front_->next;  // move front to be next in queue
+      tempData = temp->data;  // get the data that was dequeued
+      delete temp;  // delete temp node
+      return tempData;  // return tempdata
     }
   }
 
+  // pre-condition:  queue must not be empty
+  // post-condition: Bool to show if queue is empty or not
   virtual bool isEmpty() const override{
     if(this->front_ == nullptr){
       return true;
@@ -74,14 +86,19 @@ public:
     }
   }
 
+  // pre-condition:  queue must not be empty
+  // post-condition: prints out the contents of hte queue.
   virtual void print() const override{
-    Node* temp = new Node;
-    temp = this->front_;
+    Node* temp = new Node;  // temp node
+    temp = this->front_;  // set temp to the front node
+    // if front is null then the queue is empty
     if(this->front_ == nullptr){
       cout << "The LLQueue is empty!" << endl;
     }else{
       cout << "[ ";
+      //while not at end
       while(temp!=nullptr){
+        // print out the data
         cout << temp->data << " ";
         temp = temp->next;
       }
--- Assignments/2/Q2a/main.cpp
+++ Assignments/2/Q2a/main.cpp
@@ -6,29 +6,37 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   LLQueue* queue = new LLQueue();
 
-  queue->enqueue(2);
-  queue->enqueue(3);
-  queue->enqueue(1);
+  queue->enqueue(2); // add to queue
+  queue->enqueue(3); // add to queue
+  queue->enqueue(1);  // add to queue
   queue->print(); // 231
-  queue->enqueue(6);
-  queue->enqueue(5);
+  queue->enqueue(6);  // add to queue
+  queue->enqueue(5);  // add to queue
   queue->print(); //23165
-  cout << queue->dequeue() << " has been dequeued! " << endl;
-  cout << queue->dequeue() << " has been dequeued! " << endl;
+  cout << queue->dequeue() << " has been dequeued! " << endl; //remove from queue
+  cout << queue->dequeue() << " has been dequeued! " << endl; //remove from queue
   queue->print(); //165
+  //find if queue is empty
   if(queue->isEmpty()){
     cout << "LLQueue is empty!" <<endl;
   }else{
     cout << "LLQueue is not empty!" <<endl;
   }
-  cout << queue->dequeue() << " has been dequeued! " << endl;
-  cout << queue->dequeue() << " has been dequeued! " << endl;
-  cout << queue->dequeue() << " has been dequeued! " << endl;
+  queue->enqueue(8);  // add to queue
+  queue->print();
+  cout << queue->dequeue() << " has been dequeued! " << endl; //remove from queue
+  cout << queue->dequeue() << " has been dequeued! " << endl; //remove from queue
+  cout << queue->dequeue() << " has been dequeued! " << endl;  //remove from queue
+  cout << queue->dequeue() << " has been dequeued! " << endl; //remove from queue
+  queue->print();
+  //find if queue is empty
   if(queue->isEmpty()){
     cout << "LLQueue is empty!" <<endl;
   }else{
     cout << "LLQueue is not empty!" <<endl;
   }
+
+  delete queue; //done with queue so deallocate
 
   return 0;
 }
